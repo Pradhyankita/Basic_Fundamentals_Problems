87831df2e672f96ef97597f751cbe3247f4bb95f
pradhyankita1 <rapenzelpb@gmail.com>
2022-11-12 07:34:26 +0530

    Adding: Program to display sum of command prompt arguments

--- src/com/bridgelabz/pp1/SumInvalidInteger.java
+++ src/com/bridgelabz/pp1/SumInvalidInteger.java
@@ -4,15 +4,32 @@ import java.util.Scanner;
 
 public class SumInvalidInteger {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter three numbers");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        int sum = num1 + num2 + num3;
-        System.out.println("Sum of the integers is " + sum);
-        //        for (int i = 0; i < args.length; i++) {
-//            System.out.println("Arguments" + args[i]);
-//        }
+
+        System.out.println(args[0] + args[1] + args[2]);
+        for (int i = 0; i < args.length; i++) {
+            int sum = 0;
+            //sum = sum + args[i];
+            System.out.println(args[i]);
+        }
+//        System.out.println(sum(10, 20));
+//        System.out.println(sum(10, 20, 30));
+//        System.out.println(sum(10, 20, 30, 40));
+//        System.out.println(sum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
     }
 }
+
+//        System.out.println("Length: " + inputs.length);
+//        int sum = 0;
+//        for (int i = 0; i < inputs.length; i++) {
+//            sum += inputs[i];
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter three numbers");
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        int num3 = sc.nextInt();
+//        int sum = num1 + num2 + num3;
+//        System.out.println("Sum of the integers is " + sum);
+//
+
